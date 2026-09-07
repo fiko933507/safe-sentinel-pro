86ea25ac510a480a64d97c767b1fdf037059b76c
fiko933507 <f86187063@gmail.com>
2026-09-07 14:07:20 +0300

    Enable production structural fix pass

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -4,6 +4,7 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      './scripts/babel-structural-fixes.cjs',
       './scripts/babel-supported-languages.cjs',
       './scripts/babel-play-store-policy.cjs'
     ],
