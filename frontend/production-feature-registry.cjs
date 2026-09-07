d32b6d46176fcac01a1bb0949e4d02f7cc716009
fiko933507 <f86187063@gmail.com>
2026-09-07 15:47:26 +0300

    Use one source of truth for production feature policy

--- frontend/production-feature-registry.cjs
+++ frontend/production-feature-registry.cjs
@@ -1,43 +1,11 @@
 'use strict';
 
-const SUPPORTED_PRODUCTION_LANGUAGES = Object.freeze(['tr', 'en']);
+const policy = require('./production-feature-registry.json');
 
-const HIDDEN_PRODUCTION_MODULES = Object.freeze([
-  'whaleWatchView',
-  'emergencyLockView',
-  'taxReportView',
-  'dexOrdersView',
-  'gasTimeView',
-  'deepIntelView',
-  'autoPhishView'
-]);
-
-const BACKEND_READY_UI_DEFERRED = Object.freeze([
-  'walletBehavioralFingerprint',
-  'scamDna',
-  'walletSecurityGraph',
-  'earlyWarning',
-  'whaleWatch'
-]);
-
-const CORE_PLAY_STORE_FEATURES = Object.freeze([
-  'auth',
-  'accountDeletion',
-  'walletRiskScan',
-  'manualPhishingScan',
-  'transferRecipientRisk',
-  'smartContractAnalysis',
-  'revokeCenter',
-  'portfolio',
-  'liveGas',
-  'priceAlerts',
-  'vault',
-  'whitelist',
-  'blacklist',
-  'guardian',
-  'inheritance',
-  'notifications'
-]);
+const SUPPORTED_PRODUCTION_LANGUAGES = Object.freeze([...policy.supportedProductionLanguages]);
+const HIDDEN_PRODUCTION_MODULES = Object.freeze([...policy.hiddenProductionModules]);
+const BACKEND_READY_UI_DEFERRED = Object.freeze([...policy.backendReadyUiDeferred]);
+const CORE_PLAY_STORE_FEATURES = Object.freeze([...policy.corePlayStoreFeatures]);
 
 const isProductionModuleEnabled = (moduleName) =>
   !moduleName || !HIDDEN_PRODUCTION_MODULES.includes(moduleName);
